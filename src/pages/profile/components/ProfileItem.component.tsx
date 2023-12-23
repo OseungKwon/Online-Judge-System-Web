81575e4559e61e12166e96435d61e342bbe3c0e4
OseungKwon <00osos@naver.com>
2023-12-23 22:40:02 +0900

    feat: 프로필 아이템 컴포넌트 추가

---
+++ src/pages/profile/components/ProfileItem.component.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import { Input } from '@/components';
+
+import styles from './ProfileItem.module.scss';
+
+interface ProfileItemProps<T> {
+  name: string;
+  isEditAble?: boolean;
+  item: T;
+  setItem: (item: string) => void;
+  children?: React.ReactNode;
+}
+
+function ProfileItem<T extends React.ReactNode>({ name, item, setItem, isEditAble, children }: ProfileItemProps<T>) {
+  return (
+    <div className={styles.container}>
+      <span className={styles.title}>{name}</span>
+      {isEditAble ? (
+        <Input
+          value={item as string}
+          onChange={(e) => {
+            setItem(e.target.value);
+          }}
+        />
+      ) : (
+        <>{children}</>
+      )}
+    </div>
+  );
+}
+
+export default React.memo(ProfileItem);
